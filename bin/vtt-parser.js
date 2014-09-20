4bb41ae9384b7f9b0a3307d3ea79af9132aff29f
linclark <lin.w.clark@gmail.com>
2014-09-20 16:27:22 -0400

    Add parser.

---
+++ bin/vtt-parser.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+var fs = require('fs');
+var minimist = require('minimist');
+var through2 = require('through2');
+
+var vtt = require('./../');
+
+var argv = minimist(process.argv, {
+    alias: {
+        v: 'version',
+        o: 'output'
+    },
+    boolean: ['version', 'help']
+});
+
+var headers = argv.headers && argv.headers.toString().split(argv.separator);
+var filename = argv._[2];
+
+if (argv.version) {
+    console.log(require('./../package').version);
+    process.exit(0);
+}
+var output = (argv.output && argv.output !== '-') ? fs.createWriteStream(argv.output) : process.stdout;
+
+if (filename === '-' || !filename) input = process.stdin;
+else if (fs.existsSync(filename)) input = fs.createReadStream(filename);
+else {
+    console.error('File: %s does not exist', filename);
+    process.exit(2);
+}
+
+input.pipe(vtt())
+    .pipe(through2.obj(function(obj, enc, cb) {
+        cb(null, JSON.stringify(obj, null, '\t'))
+    }))
+    .pipe(output);
